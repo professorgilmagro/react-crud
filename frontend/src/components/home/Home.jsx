a81fdcc539f8e40c5a8040ed37ffe456b5dbbc64
Gilmar Soares <gilmar.santos@gmmti.com.br>
2019-06-05 18:54:12 -0300

    Ajuste exibiçao e conteudo

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import Main from '../templates/Main'
 
 export default props =>
-    <Main icon="home" title="Início" subtitle="Segundo projeto do capítulo de React.">
+    <Main icon="home" title="Início" subtitle="Projeto React: Cadastro de usuários">
         <div className="display-4">Bem vindo!</div>
         <hr />
         <p className="mb-0">
-            Sistema para exemplificar um sistema de cadastro desenvolvido em React.
-        </p>
+            Sistema para exemplificar um sistema de cadastro desenvolvido em React:
+            </p>
+            <ol className="mt-2">
+                <li>JSON Server</li>
+                <li>React Router</li>
+                <li>Routes</li>
+                <li>Components</li>
+                <li>Fragment</li>
+                <li>Bootstrap</li>
+                <li>Responsible template</li>
+            </ol>
     </Main>
